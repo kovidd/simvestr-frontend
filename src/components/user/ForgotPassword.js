b93b00eaa4cc175beea6f25a0200ece9365dd6ae
Jihad(Jey) Meraachli <z5156156@ad.unsw.edu.au>
2020-10-11 20:50:33 +1100

    change code design from classes to hooks

--- src/components/user/ForgotPassword.js
+++ src/components/user/ForgotPassword.js
@@ -1,116 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../index.css";
 import { Grid, Box, Typography, TextField, Button } from "@material-ui/core";
 import { MainWrapper } from "../ui";
 
-const emailRegex = RegExp(
-  /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i
-);
+const ForgotPasswordForm = () => {
+  const [username, setUsername] = useState("");
+  const [usernameError, setUsernameError] = useState("");
 
-const formValid = ({ formErrors, ...rest }) => {
-  let valid = true;
-
-  // validate form errors being empty
-  Object.values(formErrors).forEach((val) => {
-    val.length > 0 && (valid = false);
-  });
-
-  // validate the form was filled out
-  Object.values(rest).forEach((val) => {
-    val === null && (valid = false);
-  });
-
-  return valid;
-};
-
-class ForgotPassword extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: null,
-      formErrors: {
-        email: "",
-      },
-    };
-  }
-
-  handleBlur = (e) => {
+  const handleBlur = (e) => {
     e.preventDefault();
     const { name, value } = e.target;
-    let formErrors = { ...this.state.formErrors };
 
-    formErrors.email = emailRegex.test(value)
-      ? ""
-      : "Email address is invalid.";
-
-    this.setState({ formErrors, [name]: value }, () => console.log(this.state));
+    switch (name) {
+      case "username":
+        setUsername(username);
+        value.length < 8
+          ? setUsernameError("Username must be at least 8 characters.")
+          : setUsernameError("");
+        break;
+      default:
+        break;
+    }
   };
 
-  handleSubmit = (e) => {
+  const handleChange = (e) => {
     e.preventDefault();
     const { name, value } = e.target;
-    let formErrors = { ...this.state.formErrors };
 
-    if (formValid(this.state)) {
+    switch (name) {
+      case "username":
+        setUsername(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setUsername(username);
+    setUsernameError(usernameError);
+
+    if (username && !usernameError) {
       console.log(`
-        --SUBMITTING--
-        Email: ${this.state.email}`);
+      --SUBMITTING--
+      Username: ${username}`);
     } else {
       console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
-      if (this.state.email === null) {
-        formErrors.email = "Email address is required.";
+      if (!username) {
+        setUsernameError("Username is required.");
       }
     }
-    this.setState({ formErrors, [name]: value }, () => console.log(this.state));
   };
 
-  render() {
-    const { formErrors } = this.state;
-
-    return (
-      <MainWrapper>
-        <Box
-          display="flex"
-          height="100%"
-          flexDirection="column"
-          alignItems="center"
-          p="2rem"
+  return (
+    <form>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            name="username"
+            label="Username"
+            value={username}
+            className={usernameError.length > 0 ? "error" : null}
+            onChange={(e) => handleChange(e, "username")}
+            onBlur={(e) => handleBlur(e, "username")}
+            fullWidth
+          />
+          {usernameError.length > 0 && (
+            <Box className="errorMessage">{usernameError}</Box>
+          )}
+        </Grid>
+      </Grid>
+      <Box display="flex" justifyContent="center">
+        <Button
+          className="btn"
+          variant="contained"
+          color="primary"
+          onClick={handleSubmit}
         >
-          <Typography variant="h2">Simvstr</Typography>
-          <Typography varaint="body2" align="center">
-            Enter your email address and we will send through a reset link.
-          </Typography>
-          <form>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  name="email"
-                  label="Email"
-                  noValidate
-                  className={formErrors.email.length > 0 ? "error" : null}
-                  onBlur={this.handleBlur}
-                  fullWidth
-                />
-                {formErrors.email.length > 0 && (
-                  <Box className="errorMessage">{formErrors.email}</Box>
-                )}
-              </Grid>
-            </Grid>
-            <Box display="flex" justifyContent="center">
-              <Button
-                className="btn"
-                variant="contained"
-                color="primary"
-                onClick={this.handleSubmit}
-              >
-                Send Request
-              </Button>
-            </Box>
-          </form>
-        </Box>
-      </MainWrapper>
-    );
-  }
-}
+          Send Request
+        </Button>
+      </Box>
+    </form>
+  );
+};
 
-export default ForgotPassword;
+export const ForgotPassword = () => {
+  return (
+    <MainWrapper>
+      <Box
+        display="flex"
+        height="100%"
+        flexDirection="column"
+        alignItems="center"
+        p="2rem"
+      >
+        <Typography variant="h2">Simvstr</Typography>
+        <Typography varaint="body2" align="center">
+          Enter your username and we will send through a reset link.
+        </Typography>
+        <ForgotPasswordForm />
+      </Box>
+    </MainWrapper>
+  );
+};
--- src/components/user/SignupSuccess.js
+++ src/components/user/SignupSuccess.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../index.css";
 import {
   Grid,
@@ -12,180 +12,169 @@ import {
 } from "@material-ui/core";
 import { MainWrapper } from "../ui";
 
-const emailRegex = RegExp(
-  /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i
-);
+const SignupSuccessForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [enabledRememberMeCheckBox, setEnabledRememberMeCheckBox] = useState(
+    false
+  );
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
-const formValid = ({ formErrors, ...rest }) => {
-  let valid = true;
-
-  // validate form errors being empty
-  Object.values(formErrors).forEach((val) => {
-    val.length > 0 && (valid = false);
-  });
-
-  // validate the form was filled out
-  Object.values(rest).forEach((val) => {
-    val === null && (valid = false);
-  });
-
-  return valid;
-};
-
-class SignupSuccess extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: null,
-      password: null,
-      enabledRememberMeCheckBox: false,
-      formErrors: {
-        email: "",
-        password: "",
-      },
-    };
-  }
-
-  handleBlur = (e) => {
+  const handleBlur = (e) => {
     e.preventDefault();
     const { name, value } = e.target;
-    let formErrors = { ...this.state.formErrors };
 
     switch (name) {
-      case "email":
-        formErrors.email = emailRegex.test(value)
-          ? ""
-          : "Email address is invalid.";
+      case "username":
+        setUsername(username);
+        value.length < 8
+          ? setUsernameError("Username must be at least 8 characters.")
+          : setUsernameError("");
         break;
       case "password":
-        formErrors.password =
-          value.length < 8 ? "Password must be at least 8 characters." : "";
+        setPassword(password);
+        value.length < 8
+          ? setPasswordError("Password must be at least 8 characters.")
+          : setPasswordError("");
         break;
       default:
         break;
     }
-
-    this.setState({ formErrors, [name]: value }, () => console.log(this.state));
   };
 
-  handleSubmit = (e) => {
+  const handleChange = (e) => {
     e.preventDefault();
     const { name, value } = e.target;
-    let formErrors = { ...this.state.formErrors };
 
-    if (formValid(this.state)) {
-      console.log(`
-        --SUBMITTING--
-        Email: ${this.state.email}
-        Password: ${this.state.password}
-      `);
-      localStorage.setItem("rememberMe", this.state.enabledRememberMeCheckBox);
+    switch (name) {
+      case "username":
+        setUsername(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setUsername(username);
+    setPassword(password);
+    setUsernameError(usernameError);
+    setPasswordError(passwordError);
+
+    if (username && password && !usernameError && !passwordError) {
+      console.log(
+        `--SUBMITTING-- 
+        Username: ${username} 
+        Password: ${password}`
+      );
+      localStorage.setItem("rememberMe", enabledRememberMeCheckBox);
       localStorage.setItem(
-        "email",
-        this.state.enabledRememberMeCheckBox ? this.state.email : ""
+        "username",
+        enabledRememberMeCheckBox ? username : ""
       );
       localStorage.setItem(
         "password",
-        this.state.enabledRememberMeCheckBox ? this.state.password : ""
+        enabledRememberMeCheckBox ? password : ""
       );
     } else {
       console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
-      if (this.state.email === null) {
-        formErrors.email = "Email address is required.";
+      if (!username) {
+        setUsernameError("username is required.");
       }
-      if (this.state.password === null) {
-        formErrors.password = "Password is required.";
+      if (!password) {
+        setPasswordError("Password is required.");
       }
     }
-    this.setState({ formErrors, [name]: value }, () => console.log(this.state));
   };
 
-  handleRememberMeClick = () => {
-    let { enabledRememberMeCheckBox } = this.state;
-
-    this.setState({
-      enabledRememberMeCheckBox: !enabledRememberMeCheckBox,
-    });
+  const handleRememberMeClick = (e) => {
+    e.persist();
+    setEnabledRememberMeCheckBox(!enabledRememberMeCheckBox);
   };
 
-  render() {
-    const { formErrors } = this.state;
-
-    return (
-      <MainWrapper>
-        <Box
-          display="flex"
-          height="100%"
-          flexDirection="column"
-          alignItems="center"
-          p="2rem"
-        >
-          <Typography color="primary" variant="h5">
-            Signup successful! Please login.
-          </Typography>
-          <Typography variant="h2">Simvstr</Typography>
-          <Typography varaint="body2">
-            Welcome, please login to your account.
-          </Typography>
-          <form>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  name="email"
-                  label="Email"
-                  noValidate
-                  className={formErrors.email.length > 0 ? "error" : null}
-                  onBlur={this.handleBlur}
-                  fullWidth
-                />
-                {formErrors.email.length > 0 && (
-                  <Box className="errorMessage">{formErrors.email}</Box>
-                )}
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  name="password"
-                  type="password"
-                  label="Password"
-                  noValidate
-                  className={formErrors.password.length > 0 ? "error" : null}
-                  onBlur={this.handleBlur}
-                  fullWidth
-                />
-                {formErrors.password.length > 0 && (
-                  <Box className="errorMessage">{formErrors.password}</Box>
-                )}
-                <Box display="flex" justifyContent="flex-end">
-                  <Link href="./forgotpassword">Forgot Password?</Link>
-                </Box>
-              </Grid>
-              <Box display="flex" flexDirection="column">
-                <FormControlLabel
-                  control={<Checkbox name="rememberMe" color="primary" />}
-                  label="Remember me"
-                  onChange={this.handleRememberMeClick}
-                />
-              </Box>
-            </Grid>
-            <Box display="flex" justifyContent="center">
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={this.handleSubmit}
-              >
-                Login
-              </Button>
-            </Box>
-            <Box
-              display="flex"
-              flexDirection="row"
-              justifyContent="flex-start"
-            ></Box>
-          </form>
+  return (
+    <form>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            name="username"
+            label="Username"
+            value={username}
+            className={usernameError.length > 0 ? "error" : null}
+            onChange={(e) => handleChange(e, "username")}
+            onBlur={(e) => handleBlur(e, "username")}
+            fullWidth
+          />
+          {usernameError.length > 0 && (
+            <Box className="errorMessage">{usernameError}</Box>
+          )}
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            name="password"
+            type="password"
+            label="Password"
+            value={password}
+            className={passwordError.length > 0 ? "error" : null}
+            onChange={(e) => handleChange(e, "password")}
+            onBlur={(e) => handleBlur(e, "password")}
+            fullWidth
+          />
+          {passwordError.length > 0 && (
+            <Box className="errorMessage">{passwordError}</Box>
+          )}
+          <Box display="flex" justifyContent="flex-end">
+            <Link href="./forgotpassword">Forgot Password?</Link>
+          </Box>
+        </Grid>
+        <Box display="flex" flexDirection="column">
+          <FormControlLabel
+            control={<Checkbox name="rememberMe" color="primary" />}
+            label="Remember me"
+            checked={enabledRememberMeCheckBox}
+            onClick={handleRememberMeClick}
+          />
         </Box>
-      </MainWrapper>
-    );
-  }
-}
+      </Grid>
+      <Box display="flex" justifyContent="center">
+        <Button variant="contained" color="primary" onClick={handleSubmit}>
+          Login
+        </Button>
+      </Box>
+      <Box display="flex" flexDirection="row" justifyContent="flex-start">
+        <Typography>
+          Not a member? <Link href="./signup">Sign Up</Link>
+        </Typography>
+      </Box>
+    </form>
+  );
+};
 
-export default SignupSuccess;
+export const SignupSuccess = () => {
+  return (
+    <MainWrapper>
+      <Box
+        display="flex"
+        height="100%"
+        flexDirection="column"
+        alignItems="center"
+        p="2rem"
+      >
+        <Typography color="primary" variant="h5">
+          Signup successful! Please login.
+        </Typography>
+        <Typography variant="h2">Simvstr</Typography>
+        <Typography varaint="body2">
+          Welcome, please login to your account.
+        </Typography>
+        <SignupSuccessForm />
+      </Box>
+    </MainWrapper>
+  );
+};
