9daa68aac9dd0f51c228d2692bc06ea99980e519
Tim Brunette <timothybrunette@gmail.com>
2020-10-18 21:46:29 +1100

    add initial scaffold for stock search results

--- src/components/stocks/StockList.js
+++ src/components/stocks/StockList.js
@@ -1,25 +1,23 @@
 import React, { useState, useEffect } from "react";
-import { Box, TextField, Typography } from "@material-ui/core";
+import { useForm } from "react-hook-form";
+import {
+  Select,
+  Box,
+  Grid,
+  TextField,
+  Typography,
+  RadioGroup,
+  FormControl,
+  FormControlLabel,
+  Radio,
+  MenuItem,
+} from "@material-ui/core";
 import { Autocomplete } from "@material-ui/lab";
 import { MainWrapper } from "../ui";
 import { stockDetails } from "../../services/stock";
 
-const stocks = [
-  { symbol: "AAPL", name: "AppleInc" },
-  { symbol: "MSFT", name: "Microsoft" },
-  { symbol: "F", name: "Ford" },
-  {
-    symbol: "C",
-    name: "Citigroup Inc",
-  },
-  { symbol: "GM", name: "General Motors" },
-  { symbol: "PG", name: "Procter & Gamle" },
-  { symbol: "XOM", name: "Exxon Mobil Corp." },
-];
-
-export const StockList = () => {
+const StockSearch = ({ setDetails }) => {
   const [search, setSearch] = useState("");
-  const [details, setDetails] = useState(null);
 
   useEffect(() => {
     async function getStockDetails(stockSymbol) {
@@ -41,48 +39,127 @@ export const StockList = () => {
       }
     }
     getStockDetails(search);
-  }, [search]);
+  }, [search, setDetails]);
+
+  return (
+    <Autocomplete
+      freeSolo
+      id="stock-search"
+      disableClearable
+      value={search}
+      onChange={(_, newValue) => setSearch(newValue)}
+      options={stocks.map((option) => option.symbol)}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Search Stocks"
+          margin="normal"
+          variant="outlined"
+          InputProps={{ ...params.InputProps, type: "search" }}
+        />
+      )}
+    />
+  );
+};
+
+const stocks = [
+  { symbol: "AAPL", name: "AppleInc" },
+  { symbol: "MSFT", name: "Microsoft" },
+  { symbol: "F", name: "Ford" },
+  {
+    symbol: "C",
+    name: "Citigroup Inc",
+  },
+  { symbol: "GM", name: "General Motors" },
+  { symbol: "PG", name: "Procter & Gamle" },
+  { symbol: "XOM", name: "Exxon Mobil Corp." },
+];
+
+const StockDetails = ({ details }) => {
+  return (
+    <Box>
+      <Typography variant="body1">Name: {details.name}</Typography>
+      <Typography variant="body1">Industry: {details.industry}</Typography>
+      <Typography variant="body1">Exchange: {details.exchange}</Typography>
+      <Typography variant="body1">
+        Current Quote Price : {details.current}{" "}
+      </Typography>
+      <Typography variant="body1">
+        Previous Close Price: {details.previous}{" "}
+      </Typography>
+      <Typography variant="body1"></Typography>
+      <Typography variant="body1">
+        <img
+          alt="Stock Logo"
+          style={{ height: "60px", marginTop: "20px" }}
+          src={details.logo}
+        />
+      </Typography>
+    </Box>
+  );
+};
+
+const StockTrade = () => {
+  const [tradeType, setTradeType] = useState("buy");
+  const [amount, setAmount] = useState(null);
+  const [amountType, setAmountType] = useState("amount");
+
+  return (
+    <Box>
+      <FormControl component="fieldset">
+        <RadioGroup
+          row
+          aria-label="trade type"
+          name="trade"
+          value={tradeType}
+          onChange={(e) => setTradeType(e.target.value)}
+        >
+          <FormControlLabel value="buy" control={<Radio />} label="Buy" />
+          <FormControlLabel value="sell" control={<Radio />} label="Sell" />
+        </RadioGroup>
+      </FormControl>
+      <Box display="flex" alignItems="baseline">
+        <FormControl>
+          <Select
+            name="amountType"
+            value={amountType}
+            onChange={(e) => setAmountType(e.target.value)}
+          >
+            <MenuItem value="amount">Amount</MenuItem>
+            <MenuItem value="total">Total</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          name="amount"
+          label="Amount"
+          fullWidth
+        />
+      </Box>
+    </Box>
+  );
+};
+export const StockList = () => {
+  const [details, setDetails] = useState(null);
 
   return (
     <MainWrapper>
-      <Autocomplete
-        freeSolo
-        id="stock-search"
-        disableClearable
-        value={search}
-        onChange={(_, newValue) => setSearch(newValue)}
-        options={stocks.map((option) => option.symbol)}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Search Stocks"
-            margin="normal"
-            variant="outlined"
-            InputProps={{ ...params.InputProps, type: "search" }}
-          />
-        )}
-      />
-      {search && details && (
-        <Box>
-          <Typography variant="body1">{`Showing search results for ${search}:`}</Typography>
-          <Typography variant="body1">Name: {details.name}</Typography>
-          <Typography variant="body1">Industry: {details.industry}</Typography>
-          <Typography variant="body1">Exchange: {details.exchange}</Typography>
-          <Typography variant="body1">
-            Current Quote Price : {details.current}{" "}
-          </Typography>
-          <Typography variant="body1">
-            Previous Close Price: {details.previous}{" "}
-          </Typography>
-          <Typography variant="body1"></Typography>
-          <Typography variant="body1">
-            <img
-              alt="Stock Logo"
-              style={{ height: "60px", marginTop: "20px" }}
-              src={details.logo}
-            />
-          </Typography>
-        </Box>
+      <StockSearch setDetails={setDetails} />
+      {details && (
+        <Grid container>
+          <Grid item xs={6}>
+            <StockDetails details={details} />
+          </Grid>
+          <Grid item xs={6}>
+            <Box>
+              <div>Stock Chart</div>
+            </Box>
+            <Box>
+              <StockTrade />
+            </Box>
+          </Grid>
+        </Grid>
       )}
     </MainWrapper>
   );
