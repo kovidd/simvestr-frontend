112ee1599946e3487f35803caacdb31050dae5a5
timbrunette <timothybrunette@gmail.com>
2020-11-11 15:47:39 +1100

    add base working for the notification

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ import {
   Switch,
 } from "react-router-dom";
 
+// Components
 import { Signup } from "./user/Signup";
 import { Login } from "./user/Login";
 import { TermsAndConditionsSignup } from "./user/TermsAndConditionsSignup";
@@ -23,12 +24,17 @@ import { StockList } from "./stocks/StockList";
 import { LeaderBoard } from "./leaderboard/LeaderBoard";
 import { WatchListSummary } from "./watchlist/WatchListSummary";
 import { WatchListDetails } from "./watchlist/WatchListDetails";
-import { AuthContext } from "../services/api";
-import { UserContext } from "../services/user";
-import { PortfolioContext } from "../services/portfolio";
 import { AccountSettings } from "./settings/AccountSettings";
 import { PersonalDetails } from "./settings/PersonalDetails";
 import { Password } from "./settings/Password";
+
+// Contexts
+import { AuthContext } from "../services/api";
+import { UserContext } from "../services/user";
+import { PortfolioContext } from "../services/portfolio";
+import { Notification, NotificationContext } from "./ui/Notification";
+
+// Routes
 import { UnauthenticatedRoute, AuthenticatedRoute } from "./Routes";
 
 const Main = () => {
@@ -48,64 +54,85 @@ const Main = () => {
     portfolio: {},
   });
 
+  const [notification, setNotification] = useState({
+    open: false,
+    message: "",
+  });
+
   const fallbackUri = auth.isAuthenticated ? "/" : "/login";
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
       <UserContext.Provider value={{ user, setUser }}>
         <PortfolioContext.Provider value={{ portfolio, setPortfolio }}>
-          <Router>
-            <Switch>
-              <Route
-                path="/terms-and-conditions-signup"
-                component={TermsAndConditionsSignup}
-              />
-              <UnauthenticatedRoute path="/signup" component={Signup} />
-              <UnauthenticatedRoute path="/login" component={Login} />
-              <UnauthenticatedRoute
-                path="/forgotpassword"
-                component={ForgotPassword}
-              />
-              <AuthenticatedRoute
-                path="/resetpassword"
-                component={ResetPassword}
-              />
-              <Route path="/stocks" component={StockList} />
-              <AuthenticatedRoute
-                exact
-                path="/watchlist"
-                component={WatchListSummary}
-              />
-              <AuthenticatedRoute
-                exact
-                path="/watchlist/:symbol"
-                component={WatchListDetails}
-              />
-              <AuthenticatedRoute path="/leaderboard" component={LeaderBoard} />
-              <AuthenticatedRoute exact path="/" component={Homepage} />
-              <AuthenticatedRoute
-                exact
-                path="/settings"
-                component={AccountSettings}
-              />
-              <AuthenticatedRoute
-                exact
-                path="/settings/personaldetails"
-                component={PersonalDetails}
-              />
-              <AuthenticatedRoute
-                exact
-                path="/settings/password"
-                component={Password}
-              />
-              <AuthenticatedRoute
-                exact
-                path="/settings/terms-and-conditions-settings"
-                component={TermsAndConditionsSettings}
-              />
-              <AuthenticatedRoute exact path="/settings/faq" component={FAQ} />
-              <Redirect to={{ pathname: fallbackUri }} />
-            </Switch>
-          </Router>
+          <NotificationContext.Provider
+            value={{ notification, setNotification }}
+          >
+            <Notification
+              open={notification.open}
+              message={notification.message}
+              handleClose={() => setNotification({ open: false, message: "" })}
+            />
+            <Router>
+              <Switch>
+                <Route
+                  path="/terms-and-conditions-signup"
+                  component={TermsAndConditionsSignup}
+                />
+                <UnauthenticatedRoute path="/signup" component={Signup} />
+                <UnauthenticatedRoute path="/login" component={Login} />
+                <UnauthenticatedRoute
+                  path="/forgotpassword"
+                  component={ForgotPassword}
+                />
+                <AuthenticatedRoute
+                  path="/resetpassword"
+                  component={ResetPassword}
+                />
+                <Route path="/stocks" component={StockList} />
+                <AuthenticatedRoute
+                  exact
+                  path="/watchlist"
+                  component={WatchListSummary}
+                />
+                <AuthenticatedRoute
+                  exact
+                  path="/watchlist/:symbol"
+                  component={WatchListDetails}
+                />
+                <AuthenticatedRoute
+                  path="/leaderboard"
+                  component={LeaderBoard}
+                />
+                <AuthenticatedRoute exact path="/" component={Homepage} />
+                <AuthenticatedRoute
+                  exact
+                  path="/settings"
+                  component={AccountSettings}
+                />
+                <AuthenticatedRoute
+                  exact
+                  path="/settings/personaldetails"
+                  component={PersonalDetails}
+                />
+                <AuthenticatedRoute
+                  exact
+                  path="/settings/password"
+                  component={Password}
+                />
+                <AuthenticatedRoute
+                  exact
+                  path="/settings/terms-and-conditions-settings"
+                  component={TermsAndConditionsSettings}
+                />
+                <AuthenticatedRoute
+                  exact
+                  path="/settings/faq"
+                  component={FAQ}
+                />
+                <Redirect to={{ pathname: fallbackUri }} />
+              </Switch>
+            </Router>
+          </NotificationContext.Provider>
         </PortfolioContext.Provider>
       </UserContext.Provider>
     </AuthContext.Provider>
